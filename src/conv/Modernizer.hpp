c4d0b76ef45f0728fba46a3f49d5c8d3fcb00076
EndlessArch <paul.hong0619@gmail.com>
2022-07-20 22:35:32 +0900

    Initial commit.

---
+++ src/conv/Modernizer.hpp
@@ -0,0 +1,7 @@
+#ifndef RCX_CONV_MODERNIZER_HPP
+#define RCX_CONV_MODERNIZER_HPP
+
+#define fn auto
+#define ret return
+
+#endif
---
+++ src/rcx.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+
+#include <conv/Modernizer.hpp>
+
+fn main(
+  [[maybe_unused]] int argc,
+  [[maybe_unused]] char * argv[]) -> int {
+  ;
+
+  
+
+  return 0;
+}
